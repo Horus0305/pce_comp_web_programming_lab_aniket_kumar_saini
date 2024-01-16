1617896bca1b19010b2c0f15c2a95c434362d143
Horus0305 <aniket.saini603@gmail.com>
2024-01-16 22:58:50 +0530

    LP

---
+++ testAnimationLandingPage/main.js
@@ -0,0 +1,55 @@
+gsap.to("#div1", {
+    duration: 0.5,
+    x : -1000,
+    scale: 0.2,
+    scrollTrigger:{
+        trigger: "#div2",
+        start: "top bottom",
+        end: "bottom top",
+        scrub: 0.5
+    }
+  });
+gsap.to("#div2", {
+    duration: 0.5,
+    x : -1000,
+    scale: 0.2,
+    scrollTrigger:{
+        trigger: "#div3",
+        start: "top bottom",
+        end: "bottom top",
+        scrub: 0.5
+    }
+  });
+gsap.to("#div3", {
+    duration: 0.5,
+    x : -1000,
+    scale: 0.2,
+    scrollTrigger:{
+        trigger: "#div4",
+        start: "top bottom",
+        end: "bottom top",
+        scrub: 0.5
+    }
+  });
+gsap.to("#div4", {
+    duration: 0.5,
+    x : -1000,
+    scale: 0.2,
+    scrollTrigger:{
+        trigger: "#div5",
+        start: "top bottom",
+        end: "bottom top",
+        scrub: 0.5
+    }
+  });
+gsap.to("#div5", {
+    duration: 0.5,
+    x : -1000,
+    scale: 0.2,
+    scrollTrigger:{
+        trigger: "#div6",
+        start: "top bottom",
+        end: "bottom top",
+        scrub: 0.5
+    }
+  });
