12b38074a5a9bdf242e13c646e23bdb613257cd0
Soumendu22 <smanna22comp@student.mes.ac.in>
2024-01-22 17:33:24 +0530

    Modified LAnding Page

--- testAnimationLandingPage/bg.js
+++ testAnimationLandingPage/bg.js
@@ -1,11 +1,12 @@
 particlesJS("particles-js", {
   particles: {
-    number: { value: 500, density: { enable: true, value_area: 1000 } },
+    number: { value: 160, density: { enable: true, value_area: 800 } },
     color: { value: "#ffffff" },
     shape: {
       type: "circle",
       stroke: { width: 0, color: "#000000" },
       polygon: { nb_sides: 5 },
+      image: { src: "img/github.svg", width: 100, height: 100 },
     },
     opacity: {
       value: 1,
@@ -13,7 +14,7 @@ particlesJS("particles-js", {
       anim: { enable: true, speed: 1, opacity_min: 0, sync: false },
     },
     size: {
-      value: 2.5,
+      value: 3,
       random: true,
       anim: { enable: false, speed: 4, size_min: 0.3, sync: false },
     },
@@ -26,7 +27,7 @@ particlesJS("particles-js", {
     },
     move: {
       enable: true,
-      speed: 2,
+      speed: 1,
       direction: "none",
       random: true,
       straight: false,
@@ -44,7 +45,7 @@ particlesJS("particles-js", {
     },
     modes: {
       grab: { distance: 400, line_linked: { opacity: 1 } },
-      bubble: { distance: 150, size: 0, duration: 2, opacity: 0, speed: 3 },
+      bubble: { distance: 250, size: 0, duration: 2, opacity: 0, speed: 3 },
       repulse: { distance: 400, duration: 0.4 },
       push: { particles_nb: 4 },
       remove: { particles_nb: 2 },
