589a8695f0f0431b536f287f757a486c8356e088
Soumendu22 <smanna22comp@student.mes.ac.in>
2024-02-16 21:39:03 +0530

    base layout in includes folder

---
+++ includes/base.php
@@ -0,0 +1,37 @@
+<?php
+    include "head_links.php";
+    ?>
+<div class="nav">
+      <div class="emblem">
+        <a href="../testAnimationLandingPage/main.html">
+          <img class="logo" src="../img/favicon.png" alt="logo" />
+        </a>
+      </div>
+      <div class="widgets">
+        <div class="home">
+          <a href="">
+            <img src="../img/home.svg" alt="" />
+          </a>
+        </div>
+        <div class="chat">
+          <a href="">
+            <img src="../img/chat.svg" alt="" />
+          </a>
+        </div>
+        <div class="heart">
+          <a href="">
+            <img src="../img/heart.svg" alt="" />
+          </a>
+        </div>
+      </div>
+      <div class="notification">
+        <a href="">
+          <img src="../img/notification.svg" alt="" />
+        </a>
+      </div>
+      <div class="profile">
+        <a href="#">
+          <img class="profileimg" src="../img/profile.jpg" alt="profile-img" />
+        </a>
+      </div>
+    </div>
