df81e96278b952a809d8226cec88b5310ff19a6c
Soumendu22 <smanna22comp@student.mes.ac.in>
2024-02-17 19:12:36 +0530

    notifiaction panel

--- includes/base.php
+++ includes/base.php
@@ -9,7 +9,7 @@
       </div>
       <div class="widgets">
         <div class="home">
-          <a href="">
+          <a href="../Page2/page2.html">
             <img src="../img/home.svg" alt="" />
           </a>
         </div>
@@ -19,15 +19,13 @@
           </a>
         </div>
         <div class="heart">
-          <a href="../matchmaking/matchmaking.php">
+          <a href="../match-page/match.php">
             <img src="../img/heart.svg" alt="" />
           </a>
         </div>
       </div>
       <div class="notification">
-        <a href="../matchmaking/dailyhoroscope.php">
-          <img src="../img/notification.svg" alt="" />
-        </a>
+          <img src="../img/notification.svg" alt="" id="modalactive" onclick="opcl()"/>
       </div>
       <div class="profile">
         <a href="../profilepage/profile.html">
@@ -35,3 +33,36 @@
         </a>
       </div>
     </div>
+    <div id="modal">
+      <div id="overlay">
+        <img src="../img/cross.svg" alt="cancel" id="modalinactive" />
+      </div>
+    </div>
+    <script>
+      function opcl() {
+        let x = document.getElementById("modal");
+        if (x.style.display === "none") {
+          x.style.display = "block";
+          // let button = document.getElementById("modalactive");
+          // button.addEventListener("click", openModal);
+        } else {
+          x.style.display = "none";
+        }
+      }
+
+      let close = document.getElementById("modalinactive");
+      close.addEventListener("click", closeModal);
+
+      // function openModal() {
+      //   modal.style.display = "block";
+      // }
+
+      function closeModal() {
+        modal.style.display = "none";
+      }
+      window.addEventListener("keydown", (event) => {
+        if (event.code === "Escape") {
+          modal.style.display = "none";
+        }
+      });
+    </script>
--- match-page/match.php
+++ match-page/match.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<?php
+include "../includes/base.php"
+?>
     <link rel="stylesheet" href="css/match.css" />
-    <title>Matches</title>
-  </head>
-  <body>
-    <div class="nav"></div>
     <div class="content">
       <div class="heading">MATCHES</div>
       <div class="matches">
@@ -355,5 +349,4 @@
       </div>
     </div>
     <script src="js/match.js"></script>
-  </body>
-</html>
+
