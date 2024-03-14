d2b584f86aad66b1d6cf0ac6c0274f814e9f3a2c
Horus0305 <aniket.saini603@gmail.com>
2024-03-14 19:12:22 +0530

    match page db integeration

--- match-page/match.php
+++ match-page/match.php
@@ -5,347 +5,75 @@ include "../includes/base.php"
     <div class="content">
       <div class="heading">MATCHES</div>
       <div class="matches">
-        <div class="match-card">
-          <div class="image">
-            <img class="sign" src="img/pisces.png" alt="sign" />
-            <img class="photo" src="img/male-user.png" alt="photo" />
-          </div>
-          <div class="overview">
-            <div class="basic-info">
-              <div class="basic1">
-                <h1 id="name">Soumendu Manna</h1>
-                <p id="age">Age : 19</p>
-                <p id="sunsign">SunSign : Leo</p>
-                <p id="city">City : Thakurli</p>
-                <p id="work">Student</p>
-                <p id="chances">Compatibility : 97%</p>
+      <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'cosmicdestiny';
+
+try {
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        throw new Exception("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM male";
+    $result = $conn->query($sql);
+
+    if ($result === false) {
+        throw new Exception("Query failed: " . $conn->error);
+    }
+
+    if ($result->num_rows > 0) {
+        // Output data of each row
+        while($row = $result->fetch_assoc()) {
+            echo '
+
+            <div class="match-card">
+              <div class="image">
+                <img class="sign" src="img/pisces.png" alt="sign" />
+                <img class="photo" src="img/male-user.png" alt="photo" />
               </div>
-              <div class="basic2">
-                <h3>More about me</h3>
-                <p>A short description about Yourself : Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt debitis dolorem explicabo facilis delectus exercitationem cum, tempora veniam ratione modi atque expedita, quos laboriosam excepturi!</p>    
+              <div class="overview">
+                <div class="basic-info">
+                  <div class="basic1">
+                    <h1 id="name">'.$row["name"].'</h1>
+                    <p id="age">Age : '.$row["age"].'</p>
+                    <p id="sunsign">SunSign : '.$row["sign"].'</p>
+                    <p id="city">City : '.$row["city"].'</p>
+                    <p id="work">Student</p>
+                    <p id="chances">Compatibility : 97%</p>
+                  </div>
+                  <div class="basic2">
+                    <h3>More about me</h3>
+                    <p>'.$row["description"].'</p>    
+                  </div>
+                </div>
+                <div class="openline">"'.$row["quote"].'"</div>
               </div>
-            </div>
-            <div class="openline">"If it is to be It is up to me"</div>
-          </div>
-          <div class="decision">
-            <p>Send a Like !!</p>
-            <div class="like-button">
-              <div class="heart-bg">
-                <div class="heart-icon"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="match-card">
-          <div class="image">
-            <img class="sign" src="img/pisces.png" alt="sign" />
-            <img class="photo" src="img/male-user.png" alt="photo" />
-          </div>
-          <div class="overview">
-            <div class="basic-info">
-              <div class="basic1">
-                <h1 id="name">Soumendu Manna</h1>
-                <p id="age">Age : 19</p>
-                <p id="sunsign">SunSign : Leo</p>
-                <p id="city">City : Thakurli</p>
-                <p id="work">Student</p>
-                <p id="chances">Compatibility : 97%</p>
-              </div>
-              <div class="basic2">
-                <h3>More about me</h3>
-                <p>A short description about Yourself : Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt debitis dolorem explicabo facilis delectus exercitationem cum, tempora veniam ratione modi atque expedita, quos laboriosam excepturi!</p>    
-              </div>
-            </div>
-            <div class="openline">"If it is to be It is up to me"</div>
-          </div>
-          <div class="decision">
-            <p>Send a Like !!</p>
-            <div class="like-button">
-              <div class="heart-bg">
-                <div class="heart-icon"></div>
+              <div class="decision">
+                <p>Send a Like !!</p>
+                <div class="like-button">
+                  <div class="heart-bg">
+                    <div class="heart-icon"></div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
-        <div class="match-card">
-          <div class="image">
-            <img class="sign" src="img/pisces.png" alt="sign" />
-            <img class="photo" src="img/male-user.png" alt="photo" />
-          </div>
-          <div class="overview">
-            <div class="basic-info">
-              <div class="basic1">
-                <h1 id="name">Soumendu Manna</h1>
-                <p id="age">Age : 19</p>
-                <p id="sunsign">SunSign : Leo</p>
-                <p id="city">City : Thakurli</p>
-                <p id="work">Student</p>
-                <p id="chances">Compatibility : 97%</p>
-              </div>
-              <div class="basic2">
-                <h3>More about me</h3>
-                <p>A short description about Yourself : Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt debitis dolorem explicabo facilis delectus exercitationem cum, tempora veniam ratione modi atque expedita, quos laboriosam excepturi!</p>    
-              </div>
-            </div>
-            <div class="openline">"If it is to be It is up to me"</div>
-          </div>
-          <div class="decision">
-            <p>Send a Like !!</p>
-            <div class="like-button">
-              <div class="heart-bg">
-                <div class="heart-icon"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="match-card">
-          <div class="image">
-            <img class="sign" src="img/pisces.png" alt="sign" />
-            <img class="photo" src="img/male-user.png" alt="photo" />
-          </div>
-          <div class="overview">
-            <div class="basic-info">
-              <div class="basic1">
-                <h1 id="name">Soumendu Manna</h1>
-                <p id="age">Age : 19</p>
-                <p id="sunsign">SunSign : Leo</p>
-                <p id="city">City : Thakurli</p>
-                <p id="work">Student</p>
-                <p id="chances">Compatibility : 97%</p>
-              </div>
-              <div class="basic2">
-                <h3>More about me</h3>
-                <p>A short description about Yourself : Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt debitis dolorem explicabo facilis delectus exercitationem cum, tempora veniam ratione modi atque expedita, quos laboriosam excepturi!</p>    
-              </div>
-            </div>
-            <div class="openline">"If it is to be It is up to me"</div>
-          </div>
-          <div class="decision">
-            <p>Send a Like !!</p>
-            <div class="like-button">
-              <div class="heart-bg">
-                <div class="heart-icon"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="match-card">
-          <div class="image">
-            <img class="sign" src="img/pisces.png" alt="sign" />
-            <img class="photo" src="img/male-user.png" alt="photo" />
-          </div>
-          <div class="overview">
-            <div class="basic-info">
-              <div class="basic1">
-                <h1 id="name">Soumendu Manna</h1>
-                <p id="age">Age : 19</p>
-                <p id="sunsign">SunSign : Leo</p>
-                <p id="city">City : Thakurli</p>
-                <p id="work">Student</p>
-                <p id="chances">Compatibility : 97%</p>
-              </div>
-              <div class="basic2">
-                <h3>More about me</h3>
-                <p>A short description about Yourself : Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt debitis dolorem explicabo facilis delectus exercitationem cum, tempora veniam ratione modi atque expedita, quos laboriosam excepturi!</p>    
-              </div>
-            </div>
-            <div class="openline">"If it is to be It is up to me"</div>
-          </div>
-          <div class="decision">
-            <p>Send a Like !!</p>
-            <div class="like-button">
-              <div class="heart-bg">
-                <div class="heart-icon"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="match-card">
-          <div class="image">
-            <img class="sign" src="img/pisces.png" alt="sign" />
-            <img class="photo" src="img/male-user.png" alt="photo" />
-          </div>
-          <div class="overview">
-            <div class="basic-info">
-              <div class="basic1">
-                <h1 id="name">Soumendu Manna</h1>
-                <p id="age">Age : 19</p>
-                <p id="sunsign">SunSign : Leo</p>
-                <p id="city">City : Thakurli</p>
-                <p id="work">Student</p>
-                <p id="chances">Compatibility : 97%</p>
-              </div>
-              <div class="basic2">
-                <h3>More about me</h3>
-                <p>A short description about Yourself : Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt debitis dolorem explicabo facilis delectus exercitationem cum, tempora veniam ratione modi atque expedita, quos laboriosam excepturi!</p>    
-              </div>
-            </div>
-            <div class="openline">"If it is to be It is up to me"</div>
-          </div>
-          <div class="decision">
-            <p>Send a Like !!</p>
-            <div class="like-button">
-              <div class="heart-bg">
-                <div class="heart-icon"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="match-card">
-          <div class="image">
-            <img class="sign" src="img/pisces.png" alt="sign" />
-            <img class="photo" src="img/male-user.png" alt="photo" />
-          </div>
-          <div class="overview">
-            <div class="basic-info">
-              <div class="basic1">
-                <h1 id="name">Soumendu Manna</h1>
-                <p id="age">Age : 19</p>
-                <p id="sunsign">SunSign : Leo</p>
-                <p id="city">City : Thakurli</p>
-                <p id="work">Student</p>
-                <p id="chances">Compatibility : 97%</p>
-              </div>
-              <div class="basic2">
-                <h3>More about me</h3>
-                <p>A short description about Yourself : Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt debitis dolorem explicabo facilis delectus exercitationem cum, tempora veniam ratione modi atque expedita, quos laboriosam excepturi!</p>    
-              </div>
-            </div>
-            <div class="openline">"If it is to be It is up to me"</div>
-          </div>
-          <div class="decision">
-            <p>Send a Like !!</p>
-            <div class="like-button">
-              <div class="heart-bg">
-                <div class="heart-icon"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="match-card">
-          <div class="image">
-            <img class="sign" src="img/pisces.png" alt="sign" />
-            <img class="photo" src="img/male-user.png" alt="photo" />
-          </div>
-          <div class="overview">
-            <div class="basic-info">
-              <div class="basic1">
-                <h1 id="name">Soumendu Manna</h1>
-                <p id="age">Age : 19</p>
-                <p id="sunsign">SunSign : Leo</p>
-                <p id="city">City : Thakurli</p>
-                <p id="work">Student</p>
-                <p id="chances">Compatibility : 97%</p>
-              </div>
-              <div class="basic2">
-                <h3>More about me</h3>
-                <p>A short description about Yourself : Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt debitis dolorem explicabo facilis delectus exercitationem cum, tempora veniam ratione modi atque expedita, quos laboriosam excepturi!</p>    
-              </div>
-            </div>
-            <div class="openline">"If it is to be It is up to me"</div>
-          </div>
-          <div class="decision">
-            <p>Send a Like !!</p>
-            <div class="like-button">
-              <div class="heart-bg">
-                <div class="heart-icon"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="match-card">
-          <div class="image">
-            <img class="sign" src="img/pisces.png" alt="sign" />
-            <img class="photo" src="img/male-user.png" alt="photo" />
-          </div>
-          <div class="overview">
-            <div class="basic-info">
-              <div class="basic1">
-                <h1 id="name">Soumendu Manna</h1>
-                <p id="age">Age : 19</p>
-                <p id="sunsign">SunSign : Leo</p>
-                <p id="city">City : Thakurli</p>
-                <p id="work">Student</p>
-                <p id="chances">Compatibility : 97%</p>
-              </div>
-              <div class="basic2">
-                <h3>More about me</h3>
-                <p>A short description about Yourself : Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt debitis dolorem explicabo facilis delectus exercitationem cum, tempora veniam ratione modi atque expedita, quos laboriosam excepturi!</p>    
-              </div>
-            </div>
-            <div class="openline">"If it is to be It is up to me"</div>
-          </div>
-          <div class="decision">
-            <p>Send a Like !!</p>
-            <div class="like-button">
-              <div class="heart-bg">
-                <div class="heart-icon"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="match-card">
-          <div class="image">
-            <img class="sign" src="img/pisces.png" alt="sign" />
-            <img class="photo" src="img/male-user.png" alt="photo" />
-          </div>
-          <div class="overview">
-            <div class="basic-info">
-              <div class="basic1">
-                <h1 id="name">Soumendu Manna</h1>
-                <p id="age">Age : 19</p>
-                <p id="sunsign">SunSign : Leo</p>
-                <p id="city">City : Thakurli</p>
-                <p id="work">Student</p>
-                <p id="chances">Compatibility : 97%</p>
-              </div>
-              <div class="basic2">
-                <h3>More about me</h3>
-                <p>A short description about Yourself : Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt debitis dolorem explicabo facilis delectus exercitationem cum, tempora veniam ratione modi atque expedita, quos laboriosam excepturi!</p>    
-              </div>
-            </div>
-            <div class="openline">"If it is to be It is up to me"</div>
-          </div>
-          <div class="decision">
-            <p>Send a Like !!</p>
-            <div class="like-button">
-              <div class="heart-bg">
-                <div class="heart-icon"></div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="match-card">
-          <div class="image">
-            <img class="sign" src="img/pisces.png" alt="sign" />
-            <img class="photo" src="img/male-user.png" alt="photo" />
-          </div>
-          <div class="overview">
-            <div class="basic-info">
-              <div class="basic1">
-                <h1 id="name">Soumendu Manna</h1>
-                <p id="age">Age : 19</p>
-                <p id="sunsign">SunSign : Leo</p>
-                <p id="city">City : Thakurli</p>
-                <p id="work">Student</p>
-                <p id="chances">Compatibility : 97%</p>
-              </div>
-              <div class="basic2">
-                <h3>More about me</h3>
-                <p>A short description about Yourself : Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt debitis dolorem explicabo facilis delectus exercitationem cum, tempora veniam ratione modi atque expedita, quos laboriosam excepturi!</p>    
-              </div>
-            </div>
-            <div class="openline">"If it is to be It is up to me"</div>
-          </div>
-          <div class="decision">
-            <p>Send a Like !!</p>
-            <div class="like-button">
-              <div class="heart-bg">
-                <div class="heart-icon"></div>
-              </div>
-            </div>
-          </div>
-        </div>
+            ';
+        }
+    } else {
+        echo "0 results";
+    }
+
+    $conn->close();
+} catch (Exception $e) {
+    echo 'Error: ' . $e->getMessage();
+}
+?>
       </div>
     </div>
     <script src="js/match.js"></script>
